e20aa5c30ac44041dfb6c278671e4c50081daa3b
hiswhiteraven <hiswhiteraven@protonmail.com>
2020-02-03 20:32:29 -0500

    Modify ask method and impliment concatanion

--- widgets.rb
+++ widgets.rb
@@ -1,10 +1,9 @@
 def ask(question)
-  print question
-  answer = gets.chomp.to_i
-  puts answer
+  print question + " "
+  gets
 end
 
 puts "Welcome to the widget store!"
 
-answer = ask("How many widgets are you ordering? ")
-puts answer
+answer = ask("How many widgets are you ordering?")
+puts "You entered " + answer + " widgets"
