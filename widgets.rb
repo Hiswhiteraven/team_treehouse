0eff9e80210b5964d02aa1aa89e60ca6f7b73c69
hiswhiteraven <hiswhiteraven@protonmail.com>
2020-02-04 21:40:54 -0500

    Add .to_i method and variable to change string to integer

--- widgets.rb
+++ widgets.rb
@@ -6,6 +6,9 @@ end
 puts "Welcome to the widget store!"
 
 answer = ask("How many widgets are you ordering?")
-puts "You entered " + answer + " widgets"
+
+number = answer.to_i
+
+puts "For #{number} widgets, your total is: $#{number * 10}"
 
 p answer
